2ec02876b9325568dda94af0c5db3f7cd2d7acbc
Navya00301 <navyamaria2005@gmail.com>
2026-03-25 15:37:51 +0530

    first commit

--- app6/views.py
+++ app6/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'home-02.html')
 
-def login(request):
-    return render(request,'login.html')
+# def login(request):
+#     return render(request,'login.html')
 
-def register(request):
-    return render(request,'register.html')
+# def register(request):
+#     return render(request,'register.html')
 
 def forgotpassword(request):
     return render(request,'forgotpassword.html')
@@ -33,3 +33,57 @@ def confirmation(request):
 
 def checkout(request):
     return render(request,'checkout.html')
+
+
+
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register(request):
+    if request.method =="POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirmpassword')
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match")
+            return redirect('register') 
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request,"Username already exists")
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request,"Email already exists")
+            return redirect('register')
+
+        user= User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+        )
+        user.save()
+        messages.success(request,"Account created successfully")
+        return redirect('login')
+    return render(request,'register.html')
+
+
+from django.contrib.auth import authenticate,login
+
+def user_login(request):
+    if request.method=="POST" :
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid username or password")
+            return redirect('login')
+    return render(request,'login.html')
